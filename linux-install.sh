f5ed48fc11328e8b10d5eb113dd27488a81c7b8f
Alessandro Celso <cttynul@users.noreply.github.com>
2016-03-19 08:50:56 +0100

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -14,7 +14,7 @@ then
     	echo Removing temp files
     	rm -r -f ~/.gimpshop-temp
     	echo Installation complete! You can now run GIMPshop Reloaded!
-    	echo To display dark-theme Photoshop-like you have to select "New CS6 Themes..." under
+    	echo To display dark-theme Photoshop-like you have to select "New CS6 Themes..." in
     	echo Edit  Preferences  Themes
     	echo 
     	echo Enjoy - cttynul/zegt
@@ -22,10 +22,10 @@ then
     else
     	echo Git is not installed
     	echo Install it using your package manager
-    	echo $ sudo apt-get install git
+    	echo i.e. (buntu, debian-based) sudo apt-get install git
     fi
 else
     echo GIMP is not installed
     echo Install it using your package manager
-    echo $ sudo apt-get install gimp
+    echo i.e. (buntu, debian-based) sudo apt-get install gimp
 fi
